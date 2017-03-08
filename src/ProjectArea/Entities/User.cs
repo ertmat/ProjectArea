177cb4e530302b18fd32beaf2980878f33187db2
M. Chabiera <m.drakhu@gmail.com>
2017-03-08 17:00:11 +0100

    Added fiew fields to the user

--- src/ProjectArea/Entities/User.cs
+++ src/ProjectArea/Entities/User.cs
@@ -4,7 +4,9 @@ namespace ProjectArea.Entities
 {
     public class User : IdentityUser
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
